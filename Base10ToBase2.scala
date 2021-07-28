16498c59b4fcda1ea958318b5f7abdfad338ee03
Zeeshan Haider <zeeshanalisyed@users.noreply.github.com>
2021-07-28 13:34:09 +0400

    Update Base10ToBase2.scala

--- Base10ToBase2.scala
+++ Base10ToBase2.scala
@@ -1,16 +1,22 @@
 import scala.annotation.tailrec
 
-object Base10ToBase2 extends App {
+object Base10ToBase2 {
   val base: Int = 2;
   
-  println(toBinary(294))
 
+  def parser(str: String): String = {
+    val lentobe = 8 - str.length
+    (1 to lentobe).map(_*0).mkString + str
+  }
+  
   def toBinary(num: Int): String = {
     var result: List[Int] = List();
     // @tailrec
     def binary(n: Int): String = {
-      if (n == 1) "1" else (binary(n/base).toString) + ((n%2).toString)
+      if (n == 1) "1" 
+      else if (n < 1) "0" 
+      else (binary(n/base).toString) + ((n%2).toString)
     }
-    binary(num);
+    parser(binary(num));
   }
 }
